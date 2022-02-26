c746e613b5cd1cfadee395a8013dd1f7c9b0c340
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-02-26 11:33:41 +0100

    Update Goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -20,14 +20,29 @@ const setGoal = asyncHandler(async (req, res) => {
 		throw new Error('Please add a text field')
 	}
 
-	res.status(200).json({ message: 'Set goal' })
+	const goal = await Goal.create({
+		text: req.body.text,
+	})
+
+	res.status(200).json(goal)
 })
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: `Update goal ${req.params.id}` })
+	const goal = await Goal.findById(req.params.id)
+
+	if (!goal) {
+		res.status(400)
+		throw new Error('Goal not found')
+	}
+
+	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+	})
+
+	res.status(200).json(updatedGoal)
 })
 
 // @desc    Delete goals
